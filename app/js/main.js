0816ed864248918af50ffa089425aa5d11387b47
Egor <egor.starodumov@mail.ru>
2022-04-12 23:01:41 +0400

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -106,7 +106,7 @@ $(document).ready(function() {
     });
 
 
-    /***/
+    /*** Datepicker ***/
 
     $(".datepicker").datepicker({
         dateFormat: 'dd.mm.y',
@@ -133,24 +133,26 @@ $(document).ready(function() {
     });
 
     $(".datepicker-to").datepicker({
-            defaultDate: "+1w",
-            dateFormat: 'dd.mm.y',
-            numberOfMonths: 1,
-            closeText: 'Закрыть',
-            prevText: 'Предыдущий',
-            nextText: 'Следующий',
-            currentText: 'Сегодня',
-            monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-            monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
-            dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
-            dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
-            dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-            weekHeader: 'Не',
-            firstDay: 1,
-            isRTL: false,
-            showMonthAfterYear: false,
-            yearSuffix: ''
-        });
+        defaultDate: "+1w",
+        dateFormat: 'dd.mm.y',
+        numberOfMonths: 1,
+        closeText: 'Закрыть',
+        prevText: 'Предыдущий',
+        nextText: 'Следующий',
+        currentText: 'Сегодня',
+        monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+        dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+        dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+        dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        weekHeader: 'Не',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    });
+
+    /*** End Datepicker ***/
 
 
     gridBtn.on("click", function(e) {
